d69e822ec3bd7abfc91665eba9a673b4b5831978
boris <boris@example.com>
2020-04-21 21:15:15 +0300

    added new files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-
+import classes from './Header.module.css';
 const Header = () => {
-    return <header className='header'>
+    return <header className={classes.header}>
     <img src='https://say-hi.me/wp-content/uploads/2018/07/mozilla-logos-1.png' />
   </header>
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
+import classes from './Navbar.module.css';
 
 const Navbar = () => {
-    return <nav className='nav'>
-    <div>
-     <a href="">Profile</a> 
+    return <nav className={classes.nav}>
+    <div className={classes.item}>
+     <a href="#">Profile</a> 
     </div>
-    <div>
-     <a href="">Messages</a> 
+    <div className={classes.item}>
+     <a href="#">Messages</a> 
     </div>
-    <div>
-     <a href="">News</a> 
+    <div className={classes.item}>
+     <a href="#">News</a> 
     </div>
-    <div>
-     <a href="">Music</a> 
+    <div className={classes.item}>
+     <a href="#">Music</a> 
     </div>
-    <div>
-     <a href="">Settings</a> 
+    <div className={classes.item}>
+     <a href="#">Settings</a> 
     </div>
     </nav>
 }
